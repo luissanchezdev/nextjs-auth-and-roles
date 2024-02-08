4cea76450e826d9a00598590fd93e70809c17a48
Luis Sanchez <luis.sanchez.dev@gmail.com>
2024-02-07 19:16:23 -0500

    feature: home with buton signin

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
 import { Button } from "@/components/ui/button";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import LoginButton from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets:["latin"],
+  weight: "600"
+})
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-4xl">Welcome</h1>
-      <Button>Click</Button>
-    </>
+    <main className="h-full flex flex-col justify-center items-center">
+      <div>
+        <h1 className={cn("text-4xl font-bold text-white shadow-md", font.className )}>Inicio de sesión</h1>
+        <LoginButton asChild>
+          <Button variant={ "secondary" }>
+            Sign in
+          </Button>
+        </LoginButton>
+      </div>
+    </main>
     
   );
 }
